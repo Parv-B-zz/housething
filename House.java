04d8d73e786db070e837e4d531f04c2fecf432ed
Parv-B <65509734+Parv-B@users.noreply.github.com>
2021-04-23 15:54:31 +0400

    .

--- House.java
+++ House.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 public class House extends person {
 
     public House(long b) {
-        personId = 1111;// id reserved for
+        personId = 1111;// id reserved for house
         money = b;
     }
 
@@ -11,8 +11,16 @@ public class House extends person {
 
     public void sell(int itemId, int personId) {
         /**
-         * itId - ID of item to be sold perId - ID of buyer
+         * itId - ID of item to be sold | perId - ID of seller
          */
+
+        if (!(items.containsKey(itemId))) {
+            System.out.println("Item does not exist");
+            return;
+        } else if (!(people.containsKey(personId))) {
+            System.out.println("Person does not exist");
+            return;
+        }
 
         // get item from hashmap
         item it = items.get(itemId);
@@ -24,12 +32,13 @@ public class House extends person {
         person temp1 = c;
 
         // attempt transaction
-        try {
-            it.owner = c;
-            this.money += it.value;
-            c.money -= it.value;
-            assert c.money >= 0 : (c.name + " cannot afford item " + it.itemId);
-        } catch (Exception e) {
+        it.owner = c;
+        this.money += it.value;
+        c.money -= it.value;
+
+        if (this.money < 0) {
+            cli.println("Transaction failed");
+            cli.println(c.name + " cannot afford item " + it.itemId);
             // return to failsafes
             it = temp;
             c = temp1;
@@ -40,6 +49,15 @@ public class House extends person {
         /**
          * itId - ID of item to be sold | perId - ID of seller
          */
+
+        if(!(items.containsKey(itemId))){
+            System.out.println("Item does not exist");
+            return;
+        }
+        else if(!(people.containsKey(personId))){
+            System.out.println("Person does not exist");
+            return;
+        }
 
         // get item from hashmap
         item it = items.get(itemId);
@@ -56,24 +74,24 @@ public class House extends person {
         c.money += it.value;
 
         if (this.money < 0) {
-            System.out.println("The house cannot afford item " + it.itemId);
+            cli.println("Transaction failed");
+            cli.println("The house cannot afford item " + it.itemId);
             // return to failsafes
             it = temp;
             c = temp1;
         }
     }
 
-    public void registerItem(int personId, int itemId, long val, String desc) {
+    public void addItem(int personId, int itemId, long val, String desc) {
         item tmp = new item();
-        person per = people.get(personId);
-        tmp.owner = per;
+        tmp.owner = people.get(personId);
         tmp.itemId = itemId;
         tmp.value = val;
         tmp.description = desc;
         items.put(tmp.itemId, tmp);
     }
 
-    public void registerPerson(int perId, String n, long d) {
+    public void addPerson(int perId, String n, long d) {
         person tmp = new person();
         tmp.personId = perId;
         tmp.name = n;
@@ -82,8 +100,11 @@ public class House extends person {
     }
 
     public boolean validateNewPersonId(int x) {
-        if(people.containsKey(x))  return true;
-        else return false;
+        if(x==1111) return false; //1111 is reserved for the house
+        return(!(people.containsKey(x)));
     }
 
+    public boolean validateNewItemId(int x) {
+        return (!(people.containsKey(x)));
+    }
 }
--- cli.java
+++ cli.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class cli{
+    static Scanner sc = new Scanner(System.in);
     static String divider = ("----------------------------------------");
     public static void mainMenu() {
         println(divider);
@@ -10,28 +12,79 @@ public class cli{
         println("4. Sell item");
         println("5. View people");
         println("6. View items");
+        println("7. Exit");
         println(divider);
     }
     public static void clear() {
         System.out.println('\u000C');
     }
     public static void printPeople(House h) {
+        cli.clear();
         for (person p : h.people.values()) {
             println(divider);
-            println(p.name + "     " + p.personId);
-            println(p.money +"");
+            println("Name: " + p.name + "     Id: " + p.personId);
+            println("Money: " + p.money);
         }
         println(divider);
     }
-    public void printItems(House h) {
+    public static void printItems(House h) {
+        cli.clear();
         for (item i : h.items.values()) {
             println(divider);
-            println(i.itemId + "     " + i.value);
-            println(i.description);
+            println("Id: " + i.itemId + "     $" + i.value);
+            println("Owner: " + i.owner.name);
+            println("Description: " + i.description);
         }
         println(divider);
     }
     public static void println (String str) {
         System.out.println(str);
+    }
+    public static void registerPerson (House h){
+        cli.clear();
+        cli.println("Enter person's name");
+        String nam = (new Scanner(System.in)).nextLine();
+        cli.println("Enter person's deposit");
+        long mon = sc.nextLong();
+        cli.println("Enter person's id");
+        int pId = sc.nextInt();
+        if (h.validateNewPersonId(pId))
+            h.addPerson(pId, nam, mon);
+        else
+            cli.println("The personId is already in use");
+    }
+    public static void registerItem(House h) {
+        cli.clear();
+        cli.println("Enter owner's ID");
+        int ownerID = sc.nextInt();
+        cli.println("Enter item's ID");
+        int itemID = sc.nextInt();
+        cli.println("Enter item's value");
+        int value = sc.nextInt();
+        cli.println("Enter item's description");
+        String desc = (new Scanner(System.in)).nextLine();
+        if (h.validateNewItemId(itemID))
+            h.addItem(ownerID, itemID, value, desc);
+        else
+            cli.println("The itemID is already in use");
+    }
+    public static void buyItem(House h){
+        System.out.println("Enter ID of item to be bought");
+        int itemID = sc.nextInt();
+        System.out.println("Enter ID of seller");
+        int sellerID = sc.nextInt();
+        h.buy(itemID, sellerID);
+    }
+    public static void sellItem(House h){
+        System.out.println("Enter ID of item to be sold");
+        int itemID = sc.nextInt();
+        System.out.println("Enter ID of buyer");
+        int buyerID = sc.nextInt();
+        h.sell(itemID, buyerID);
+    }
+    public static void exitSeq(){
+        System.out.println("Press 1 to confirm exit");
+        int a = sc.nextInt();
+        if(a == 1) System.exit(0);
     }
 }
